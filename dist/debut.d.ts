093adb3e190b851df317489f27772fc430cc9383
voqse <git@voqse.com>
2022-09-04 17:31:02 +0200

    Remove dist folder

--- dist/debut.d.ts
+++
@@ -1,5 +0,0 @@
-import { Debut } from '@debut/community-core';
-import { BaseTransport, DebutOptions } from '@debut/types';
-export declare class ExtraDebut extends Debut {
-    constructor(transport: BaseTransport, opts: DebutOptions);
-}
