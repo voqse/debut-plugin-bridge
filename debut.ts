e62123b55d89b1c5efcd791c2fff8e445d9a71d1
voqse <git@voqse.com>
2022-09-04 14:07:02 +0200

    Initiall commit

---
+++ debut.ts
@@ -0,0 +1,14 @@
+import { Debut } from '@debut/community-core';
+import { BaseTransport, Candle, DebutOptions } from '@debut/types';
+
+export class ExtraDebut extends Debut {
+    constructor(transport: BaseTransport, opts: DebutOptions) {
+        super(transport, opts);
+
+        // this.registerPlugins([]);
+    }
+
+    // public getCandle() {
+    //     return this.currentCandle;
+    // }
+}
---
+++ index.ts
@@ -0,0 +1,61 @@
+import { Candle, PluginInterface } from '@debut/types';
+import { ExtraDebut } from './debut';
+
+export interface ExtraCandlesPluginAPI {
+    extraCandles: {
+        getCandles: () => Candle[][];
+    };
+}
+
+export type ExtraCandlesPluginOptions = {
+    extraTickers: string[];
+};
+
+export function corrTransport(opts: ExtraCandlesPluginOptions): PluginInterface {
+    let extraDebuts: ExtraDebut[] = [];
+    let extraCandles: Candle[][] = []; // [0] - current candles; [1] - previous candle
+
+    return {
+        name: 'extraCandles',
+        api: {
+            getCandles: () => extraCandles,
+        },
+
+        onInit() {
+            // Get main bot config
+            const { transport, opts: debutOpts } = this.debut;
+
+            // Init additional bots for every extra ticker
+            extraDebuts = opts.extraTickers.map(
+                (ticker) => new ExtraDebut(transport, { ...debutOpts, ticker, sandbox: true }),
+            );
+        },
+
+        async onStart() {
+            // Start all the bots
+            for (const extraDebut of extraDebuts) {
+                await extraDebut.start();
+            }
+        },
+
+        async onCandle() {
+            // Get current candle for all additional tickers
+            const currCandles = extraDebuts.map((extraDebut) => extraDebut.currentCandle);
+
+            // Last one goes in array start
+            extraCandles.unshift(currCandles);
+
+            // Keep only current and previous candles
+            if (extraCandles.length === 3) {
+                extraCandles.pop();
+            }
+        },
+
+        async onDispose() {
+            // Stop all the bots
+            for (const extraDebut of extraDebuts) {
+                await extraDebut.dispose();
+            }
+        },
+    };
+}
