c5231c9991701143fedbd9b58a06bd7308999897
voqse <git@voqse.com>
2022-10-04 13:10:23 +0200

    Update deps && fix history loading

--- src/index.ts
+++ src/index.ts
@@ -39,40 +39,49 @@ export function bridgePlugin(opts: BridgePluginOptions, env?: WorkingEnv): Bridg
             get: () => Object.values(candles),
         },
 
-        async onInit() {
+        onInit() {
             log.info('Initializing plugin...');
             // Get main bot config
             const { transport, opts: debutOpts } = this.debut;
 
             // Init providers for every extra ticker
+            for (const ticker of opts.bridge) {
+                log.debug(`Creating ${ticker} provider...`);
+                providers[ticker] = new Provider(transport, { ...debutOpts, ticker, sandbox: true });
+            }
+            log.debug(`${Object.keys(providers).length} provider(s) created`);
+        },
+
+        async onLearn(days) {
+            if (!days) return;
+
             await Promise.all(
                 opts.bridge.map((ticker) => {
                     log.debug(`Creating ${ticker} provider...`);
-                    providers[ticker] = new Provider(transport, { ...debutOpts, ticker, sandbox: true });
-
                     // Load history if testing or learning mode
-                    // TODO: 1. Fix Issue: If main ticker history loaded, tester doesn't wait
-                    //       for additional loading, so plugin returns undefined candles
-                    //       2. Move cli days arg here and call init() without conditional
-                    if (testing) {
-                        providers[ticker].init();
-                    } else if (opts.learningDays) {
-                        providers[ticker].init(opts.learningDays);
-                    }
+                    // TODO: 1. Move cli days arg here and call init() without conditional
+                    return providers[ticker].init(opts.learningDays);
                 }),
             );
-            log.debug(`${Object.keys(providers).length} provider(s) created`);
         },
 
         async onStart() {
             log.info('Starting plugin...');
-            if (testing) return;
+            if (testing) {
+                await Promise.all(
+                    opts.bridge.map((ticker) => {
+                        log.debug(`Loading ${ticker} history...`);
+                        return providers[ticker].init();
+                    }),
+                );
+                return;
+            }
 
             // Start all the providers concurrently
             await Promise.all(
                 opts.bridge.map((ticker) => {
                     log.debug(`Starting ${ticker} provider...`);
-                    providers[ticker].start();
+                    return providers[ticker].start();
                 }),
             );
             log.debug(`${Object.keys(providers).length} provider(s) started`);
@@ -83,8 +92,8 @@ export function bridgePlugin(opts: BridgePluginOptions, env?: WorkingEnv): Bridg
                 const candle = providers[ticker].getCandle();
 
                 if (!candle) {
-                    log.error('Undefined candle received, please check your transport')
-                    process.exit(0)
+                    log.error('Undefined candle received, please check your transport');
+                    process.exit(0);
                 }
 
                 // Do not check candles if transport is ok
@@ -102,15 +111,15 @@ export function bridgePlugin(opts: BridgePluginOptions, env?: WorkingEnv): Bridg
         // async onTick(tick) {
         //     log.verbose('onTick: Received');
         //     log.verbose(`onTick: ${opts.ticker}:`, ...Object.values(tick));
-        //     for (const ticker of opts.candles) {
+        //     for (const ticker of opts.bridge) {
         //         log.verbose(`onTick: ${ticker}:`, ...Object.values(candles[ticker]));
         //     }
         // },
-
+        //
         // async onCandle(candle) {
         //     log.verbose('onCandle: Received');
         //     log.verbose(`onCandle: ${opts.ticker}:`, ...Object.values(candle));
-        //     for (const ticker of opts.candles) {
+        //     for (const ticker of opts.bridge) {
         //         log.verbose(`onCandle: ${ticker}:`, ...Object.values(candles[ticker]));
         //     }
         // },
@@ -123,7 +132,7 @@ export function bridgePlugin(opts: BridgePluginOptions, env?: WorkingEnv): Bridg
             await Promise.all(
                 opts.bridge.map((ticker) => {
                     log.debug(`Stop ${ticker} provider...`);
-                    providers[ticker].dispose();
+                    return providers[ticker].dispose();
                 }),
             );
             log.debug(`${Object.keys(providers).length} provider(s) stopped`);
